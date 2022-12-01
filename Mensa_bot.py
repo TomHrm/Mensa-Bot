6228d1c87449ed77e8dd43d0b0a694de1f947013
Tom Herrmann <46518958+TomHrm@users.noreply.github.com>
2022-12-01 12:58:37 +0100

    Change time to send message and test for emojis

--- Mensa_bot.py
+++ Mensa_bot.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 
 from Api_requests import *
@@ -24,24 +24,39 @@ def create_meal(meal, cantine, price):
 
 def check_for_good_stuff(BOT_TOKEN, CHAT_ID):
     response = get_speiseplan_today()
-    Header = "Der heutige Speiseplan " + datetime.now().strftime("%d.%m.%Y") + " ist: \n"
+    Header = "Der **heutige** Speiseplan " + datetime.now().strftime("%d.%m.%Y") + " ist: \n"
     vegan = ""
     vegetarian = ""
     fav = ""
     for meal in response:
         if [ele for ele in good_stuff if (ele in meal['dish'])]:
-            fav = "[FAVORITE]"
+            fav = ":star: "
         if meal['vegan'] == True:
             vegan = '[vegan] '
         if meal['vegetarian'] == True:
-            vegetarian = '[vegetarian] '
-        Header = Header + '- ' + fav + vegan + vegetarian + meal['dish'] + ' ' + (meal['price']) + "€ \n"
+            vegetarian = ':herb: '
+        Header = Header + '- ' + fav + vegan + vegetarian + meal['dish'] + ' ' + (meal['price']) + "€ \n\n"
         vegan = ""
         vegetarian = ""
         fav = ""
-        if "&" in Header:
-            Header = Header.replace("&", "und")
-    telegram_bot_sendtext(Header, BOT_TOKEN, CHAT_ID)
+    Header = Header + "Der **morgige** Speiseplan " + str(datetime.now()+timedelta(1)) + " ist: \n"
+    response2 = get_speiseplan_tomorrow()
+    for meal in response2:
+        if [ele for ele in good_stuff if (ele in meal['dish'])]:
+            fav = ":star: "
+        if meal['vegan'] == True:
+            vegan = '[vegan] '
+        if meal['vegetarian'] == True:
+            vegetarian = ":herb: "
+        Header = Header + '- ' + fav + vegan + vegetarian + meal['dish'] + ' ' + (meal['price']) + "€ \n\n"
+        vegan = ""
+        vegetarian = ""
+        fav = ""
+
+    if "&" in Header:
+        Header = Header.replace("&", "und")
+    print(Header)
+    #telegram_bot_sendtext(Header, BOT_TOKEN, CHAT_ID)
 
 
 def get_feedback():
@@ -52,7 +67,11 @@ def send_feedback(meal, feedback):
 
 
 if __name__ == '__main__':
-    check_for_good_stuff(sys.argv[1],sys.argv[2])
+    if DEBUG == True:
+        check_for_good_stuff(1, 1)
+    else:
+        check_for_good_stuff(sys.argv[1],sys.argv[2])
+
 
 
 
