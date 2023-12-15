97a14a7d79252e6355ca07dcb9add07b3cba5aaa
Tom <tom_herrmannhh@outlook.de>
2023-12-15 10:14:20 +0100

    Refactor to EarlyBird that gets executed every Morning and Create a Script that gets execute at Night to pull the Reactions

---
+++ DuskSettler.py
@@ -0,0 +1,40 @@
+
+def get_reactions(token, chat_id, message_id):
+    """
+    Get the reactions to a message.
+
+    Args:
+    token (str): Telegram Bot API token.
+    chat_id (str): Chat ID for the message.
+    message_id (str): ID of the message to get reactions for.
+
+    Returns:
+    dict: The response from the Telegram API.
+    """
+    url = f"{TELEGRAM_API_BASE_URL}{token}/getPollVoters"
+    payload = {
+        'chat_id': chat_id,
+        'message_id': message_id
+    }
+    response = requests.get(url, data=payload)
+    return response.json()
+
+def get_poll_results(token, chat_id, message_id):
+    """
+    Get the results of a poll.
+
+    Args:
+    token (str): Telegram Bot API token.
+    chat_id (str): Chat ID for the poll.
+    message_id (str): ID of the poll to get results for.
+
+    Returns:
+    dict: The response from the Telegram API.
+    """
+    url = f"{TELEGRAM_API_BASE_URL}{token}/stopPoll"
+    payload = {
+        'chat_id': chat_id,
+        'message_id': message_id
+    }
+    response = requests.get(url, data=payload)
+    return response.json()
